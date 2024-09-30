85e5dc5ce2865181e567b6a002b041f8a6454907
Daniel Saleh <dscode89@gmail.com>
2024-09-30 19:46:15 +0100

    update footer template

--- src/components/styled/Footer/FooterTemplate.tsx
+++ src/components/styled/Footer/FooterTemplate.tsx
@@ -2,12 +2,13 @@ import styled from "styled-components";
 
 export const FooterTemplate = styled.footer<{ $bgColor: string }>`
   display: flex;
-  justify-content: space-between;
+  justify-content: center;
   align-items: center;
   padding: 1.2em;
   background-color: ${(props) => props.$bgColor};
 
   @media (max-width: 850px) {
     flex-direction: column;
+    justify-content: space-between;
   }
 `;
