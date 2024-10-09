7d8938d5a7c6ba589e51110290399ae3062bec49
Daniel Saleh <dscode89@gmail.com>
2024-10-09 17:20:55 +0100

    update modal form to use atomic form element

--- src/components/molecules/Forms/ModalForm.tsx
+++ src/components/molecules/Forms/ModalForm.tsx
@@ -1,6 +1,9 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { PrimaryBtn } from "../../atoms/Buttons/PrimaryBtn";
-import { BorderBottomTextInput } from "../../styled/Forms/Inputs/BorderBottomTextInput";
+import { InputWithLabel } from "./InputWithLabel";
+import { InputFieldIconWrapper } from "../../styled/Forms/Inputs/InputFieldIconWrapper";
+import { ErrorMessage } from "../../atoms/Errors/ErrorMessage";
+import regexPatterns from "../../../utils/regexPatterns";
 
 export const ModalForm = () => {
   const [email, setEmail] = useState({
@@ -8,34 +11,75 @@ export const ModalForm = () => {
     isActive: false,
     isValid: false,
   });
+  const [submissionIsDisabled, setSubmissionIsDisabled] = useState(true);
+  const handleRegistrationFormChange = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setEmail((current) => {
+      return {
+        ...current,
+        value: e.target.value,
+        isActive: e.target.value ? true : false,
+        isValid: regexPatterns.email.test(e.target.value),
+      };
+    });
+  };
+
+  useEffect(() => {
+    if (email.isValid) {
+      setSubmissionIsDisabled(false);
+    } else {
+      setSubmissionIsDisabled(true);
+    }
+  }, [email]);
+
   return (
     <form style={{ width: "90%", padding: "0.35em" }}>
-      <fieldset
+      <InputWithLabel
+        inputId="reg-email"
+        labelFor="reg-email"
+        isRequired
+        onChange={handleRegistrationFormChange}
+        isActive={email.isActive}
+        isValid={email.isValid}
+        labelText="Email"
+      >
+        {email.isValid ? (
+          <InputFieldIconWrapper $color="#5dbea3">
+            &#10003;
+          </InputFieldIconWrapper>
+        ) : email.value !== "" ? (
+          <InputFieldIconWrapper $color="#FAA0A0">
+            &#10007;
+          </InputFieldIconWrapper>
+        ) : null}
+      </InputWithLabel>
+      {!email.isValid && email.isActive ? (
+        <>
+          <ErrorMessage>
+            That doesn't look right! Please provide a valid email.
+          </ErrorMessage>
+        </>
+      ) : null}
+
+      <div
         style={{
-          padding: "1.2em",
+          width: "100%",
           display: "flex",
-          flexDirection: "column",
           justifyContent: "center",
-          alignItems: "center",
-          border: "1px solid #D3D3D3",
+          marginTop: "0.8rem",
         }}
       >
-        <legend>Email Address</legend>
-        <BorderBottomTextInput
-          placeholder="Enter Email..."
-          $isActive={email.isActive}
-          $isValid={email.isValid}
-        />
         <PrimaryBtn
+          onClick={() => {}}
           color="white"
           bgcolor="#5dbea3"
           hoverBgColor="#7dcbb5"
-          isDisabled={false}
-          onClick={() => {}}
+          isDisabled={submissionIsDisabled}
         >
-          Join Us!
+          Submit
         </PrimaryBtn>
-      </fieldset>
+      </div>
     </form>
   );
 };
